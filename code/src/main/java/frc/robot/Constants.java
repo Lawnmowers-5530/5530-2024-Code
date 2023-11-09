b893a44b02ef7e82424268f1f746472b61827045
naackisnotavailable <nroney101@gmail.com>
2023-11-09 08:56:26 -0500

    commit?

--- code/src/main/java/frc/robot/Constants.java
+++ code/src/main/java/frc/robot/Constants.java
@@ -1,28 +1,30 @@
 package frc.robot;
 
+import edu.wpi.first.math.util.Units;
+
 public final class Constants {
-    public static final int trackWidth = 20;
-    public static final int wheelBase = 20;
+    public static final double trackWidth = Units.inchesToMeters(24);
+    public static final double wheelBase = Units.inchesToMeters(24);
 
 
     public static final class Mod0{
-        public static final int driveMotor = 2;
-        public static final int turnMotor = 1;
-        public static final int canCoder = 1;
+        public static final int driveMotor = 5;
+        public static final int turnMotor = 6;
+        public static final int canCoder = 13;
     }
     public static final class Mod1{
-        public static final int driveMotor = 1;
-        public static final int turnMotor = 1;
-        public static final int canCoder = 1;
+        public static final int driveMotor = 7;
+        public static final int turnMotor = 8;
+        public static final int canCoder = 14;
     }
     public static final class Mod2{
-        public static final int driveMotor = 1;
-        public static final int turnMotor = 1;
-        public static final int canCoder = 1;
+        public static final int driveMotor = 11;
+        public static final int turnMotor = 12;
+        public static final int canCoder = 16;
     }
     public static final class Mod3{
-        public static final int driveMotor = 1;
-        public static final int turnMotor = 1;
-        public static final int canCoder = 1;
+        public static final int driveMotor = 9;
+        public static final int turnMotor = 10;
+        public static final int canCoder = 15;
     }
 }
