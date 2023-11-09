a84a24ea46746d1c753a9472e55732cce8c8315f
naackisnotavailable <nroney101@gmail.com>
2023-11-09 09:17:57 -0500

    hi

--- code/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ code/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -8,19 +8,23 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.annotations.Config;
+import io.github.oblarg.oblog.annotations.Log;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import com.ctre.phoenix.sensors.CANCoder;
 
-public class SwerveModule extends SubsystemBase {
-
-  PIDController anglePID = new PIDController(.5, .1, 0);
-
+public class SwerveModule extends SubsystemBase{
+  PIDController anglePID = new PIDController(0.5, 0, 0);
+  @Log
+  private double pidOut;
   CANSparkMax drive;
   CANSparkMax rotate;
   RelativeEncoder encoder;
+  CANCoder canCoder;
 
   public SwerveModule(int driveMotorID, int turnMotorID, int canCoderID) {
     drive = new CANSparkMax(driveMotorID, MotorType.kBrushless);
@@ -28,15 +32,28 @@ public class SwerveModule extends SubsystemBase {
     CANCoder canCoder = new CANCoder(canCoderID);
     encoder = rotate.getEncoder();
     encoder.setPosition(canCoder.getAbsolutePosition());
+    anglePID.enableContinuousInput(-Math.PI, Math.PI); //its a wheel
+    encoder.setPositionConversionFactor(360); //convert rotations to degrees
+    
   }
   public void setState(SwerveModuleState state){
-      state = SwerveModuleState.optimize(state, new Rotation2d(encoder.getPosition()));
-      
-        drive.set(state.speedMetersPerSecond);
-        rotate.set(anglePID.calculate(encoder.getPosition(), state.angle.getDegrees()));
+      state = SwerveModuleState.optimize(state, new Rotation2d(canCoder.getAbsolutePosition()));
+
+        drive.set(state.speedMetersPerSecond/2.5);
+        pidOut = anglePID.calculate(encoder.getPosition(), state.angle.getDegrees());
+        rotate.set(pidOut);
+  }
+
+  public double getTurningPosition() {
+      return encoder.getPosition();
+      //return getAbsoluteEncoderRad();
     }
+  public double getTurningVelocity() {
+      return encoder.getVelocity();
+    }
+    
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // This method will be called once per scheduler ru n
   }
 }
