7779b5b2c39ce73a915c1708d407d61c719080c9
Hugo Wang <hwang@greenhillsschool.org>
2024-03-09 19:53:41 +0000

    update led code

--- code/src/main/java/frc/robot/subsystems/Camera.java
+++ code/src/main/java/frc/robot/subsystems/Camera.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import org.opencv.core.Mat;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CameraServerCvJNI;
 import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.UsbCamera;
@@ -18,19 +19,18 @@ public class Camera extends SubsystemBase {
     CvSource output;
     
     public Camera( String name, int port, int width, int height, int fps) {
-        System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
-        System.loadLibrary("/usr/local/frc/third-party/lib/libopencv_java480.so");
         camera = CameraServer.startAutomaticCapture(name, port);
+
         camera.setVideoMode(PixelFormat.kMJPEG, width, height, fps);
-        sink = CameraServer.getVideo(camera);
+        /**sink = CameraServer.getVideo(camera);
         output = CameraServer.putVideo(name, width, height);
         image = new Mat();
-        CommandScheduler.getInstance().registerSubsystem(this);
+        CommandScheduler.getInstance().registerSubsystem(this);**/
     }
 
     @Override
     public void periodic() {
-        if (!camera.isConnected()) {
+        /**if (!camera.isConnected()) {
             System.out.println("no cam disconnected");
             return;
         }
@@ -39,6 +39,6 @@ public class Camera extends SubsystemBase {
             System.out.println("no cam");
             return;
         }
-        output.putFrame(image);
+        output.putFrame(image);**/
     }
 }
