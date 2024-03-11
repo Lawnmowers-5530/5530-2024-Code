c5e67ebccadaf336429b844b05cb2174698a337a
Hugo Wang <hwang@greenhillsschool.org>
2024-03-11 19:50:02 +0000

    complete ledmanager

--- code/src/main/java/frc/robot/subsystems/LedManager.java
+++ code/src/main/java/frc/robot/subsystems/LedManager.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LEDConstants.Patterns;
+import frc.robot.Constants.LEDConstants.Priorities;
 import frc.robot.data.GlobalState;
 import frc.robot.subsystems.LedController_MultiAccess.LedControllerProxy;
 
@@ -9,10 +12,22 @@ public class LedManager extends SubsystemBase {
 
     public LedManager(LedControllerProxy ledcontroller) {
         this.ledcontroller = ledcontroller;
+        CommandScheduler.getInstance().registerSubsystem(this);
     }
 
     @Override
     public void periodic() {
-
+        if (!GlobalState.noteLoaded && GlobalState.armReady) {
+            ledcontroller.setPattern(Patterns.NO_NOTE_ARM_READY, Priorities.MEDIUM);
+        }
+        if (GlobalState.noteLoaded) {
+            ledcontroller.setPattern(Patterns.NOTE_LOADED, Priorities.HIGH);
+        }
+        if (GlobalState.noteLoaded && GlobalState.inSpeakerZone) {
+            ledcontroller.setPattern(Patterns.NOTE_LOADED_IN_ZONE, Priorities.MEDIUM);
+        }
+        if (!GlobalState.noteLoaded && !GlobalState.armReady) {
+            ledcontroller.setPattern(Patterns.NO_NOTE_ARM_NOT_READY, Priorities.MEDIUM);
+        }
     }
 }
