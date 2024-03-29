089a1f41b1981eb5af1bd4c0f14f1c04aed09b6d
Smee404 <sameerkheterpal03@gmail.com>
2024-03-29 19:56:13 -0400

    end friday

--- code/src/main/java/frc/robot/subsystems/DistanceSensor.java
+++ code/src/main/java/frc/robot/subsystems/DistanceSensor.java
@@ -14,14 +14,18 @@ public class DistanceSensor extends SubsystemBase implements Loggable{
     Rev2mDistanceSensor sensor;
 
     public DistanceSensor() {
-        sensor = new Rev2mDistanceSensor(Port.kMXP);
+        sensor = new Rev2mDistanceSensor(Port.kOnboard);
         sensor.setDistanceUnits(Rev2mDistanceSensor.Unit.kMillimeters);
         Shuffleboard.getTab("DistanceSensor").addNumber("distance", this::getDistance);
         this.setEnabled(true);
     }
 
     public double getDistance() { // returns distance in mm
-        return sensor.getRange();
+        if (sensor.getRange() == -1) {
+            return 400;
+        } else {
+            return sensor.getRange();
+        }
     }
 
     public void setEnabled(boolean enabled) {
