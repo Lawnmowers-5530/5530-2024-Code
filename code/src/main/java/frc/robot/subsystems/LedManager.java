70f6190832e24951a196b1b9accd2818d902e379
Hugo Wang <hwang@greenhillsschool.org>
2024-03-30 14:09:04 +0000

    final bits of cleanup

--- code/src/main/java/frc/robot/subsystems/LedManager.java
+++ code/src/main/java/frc/robot/subsystems/LedManager.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import java.util.function.BooleanSupplier;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
