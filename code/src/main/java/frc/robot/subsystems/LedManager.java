40854d98477a4b2805cc6894281a225176a17867
Hugo Wang <hwang@greenhillsschool.org>
2024-03-30 14:39:44 +0000

    more cleanup

--- code/src/main/java/frc/robot/subsystems/LedManager.java
+++ code/src/main/java/frc/robot/subsystems/LedManager.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+
 import static frc.robot.Constants.LedConstants.*;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.LedController_MultiAccess.LedControllerProxy;
